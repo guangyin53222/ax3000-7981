a90467beff4046e7f48a25e2812ae02cc67c2863
guangyin53222 <532225662@qq.com>
2026-09-12 18:03:42 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,8 +4,8 @@
 # Target: immortalwrt-mt798x-rebase @ 25.12
 #
 
-# ===================== 修改默认 IP → 192.168.66.1 =====================
-sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
+# ===================== 修改默认 IP → 192.168.100.1 =====================
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # ===================== 修改主机名 =====================
 sed -i 's/ImmortalWrt/ARWRT/g' package/base-files/files/bin/config_generate
@@ -26,11 +26,38 @@ sed -i 's/^CONFIG_OPENCLASH_CORE_TYPE=.*/CONFIG_OPENCLASH_CORE_TYPE="binary"/' .
 # ===================== 关闭 YJIT（防止 OOM） =====================
 sed -i 's/^CONFIG_RUBY_ENABLE_YJIT=y/# CONFIG_RUBY_ENABLE_YJIT is not set/' .config
 
-# ===================== VNT2：拉取LuCI管理面板，核心二进制手动放入files =====================
+# ===================== VNT2：拉取LuCI网页插件 =====================
 rm -rf package/luci-app-vnt2
 git clone https://github.com/guangyin53222/luci-app-vnt2 package/luci-app-vnt2
 
-# 给files内VNT二进制补齐执行权限【关键，防止windows上传丢失权限】
+# 给files内VNT二进制补齐执行权限【Windows上传丢失权限修复】
 chmod +x files/usr/bin/vnt2_cli files/usr/bin/vnt2_ctrl
+# uci-defaults配置文件权限
+chmod +x files/etc/uci-defaults/99-vnt-init
+
+# ========== 固化VNT防火墙规则（刷完固件自动添加vnt防火墙zone，解决飞牛连不上的核心坑） ==========
+cat << 'EOF' > files/etc/uci-defaults/99-vnt-firewall
+#!/bin/sh
+# 新建vnt防火墙区域
+uci add firewall zone
+uci set firewall.@zone[-1].name='vnt'
+uci set firewall.@zone[-1].network='tun-vnt'
+uci set firewall.@zone[-1].input='ACCEPT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='ACCEPT'
+
+# LAN <-> VNT双向转发
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='lan'
+uci set firewall.@forwarding[-1].dest='vnt'
+
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='vnt'
+uci set firewall.@forwarding[-1].dest='lan'
+
+uci commit firewall
+exit 0
+EOF
+chmod +x files/etc/uci-defaults/99-vnt-firewall
 
 echo "diy-part2.sh done."
